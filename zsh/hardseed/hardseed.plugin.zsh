0821192a6957c937ee465edd9baa368d4cc9e6d8
ZhangRGK <stamen0630@gmail.com>
2016-10-25 16:58:53 +0800

    rm zsh

--- zsh/hardseed/hardseed.plugin.zsh
+++
@@ -1,5 +0,0 @@
-alias hd=hardseed
-alias hds='hardseed --topics-range 1 64 --saveas-path ~/hardseeds --timeout-download-picture 600 --av-class caoliu_asia_mosaicked_reposted --hate 重口味 孕妇 --proxy socks5://127.0.0.1:1080'
-alias arc='aria2c --conf-path="/Users/ken/hardseeds/aria2.conf" -D'
-alias mcat=imgcat
-alias mls=imgls
